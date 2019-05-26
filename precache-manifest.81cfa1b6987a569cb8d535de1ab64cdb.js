692554e47e731bdef1e9895cfd8669db83624613
David Weid <david.weid.2@gmail.com>
2019-05-26 18:49:34 -0400

    Updates

--- precache-manifest.81cfa1b6987a569cb8d535de1ab64cdb.js
+++ precache-manifest.81cfa1b6987a569cb8d535de1ab64cdb.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8c4e8b429cc33617cf077f10cdbd66fa",
+    "revision": "a84fce815bd2fe7b0bb252080698c4e9",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "bdd3ff9ef296bcf7e793",
-    "url": "/portfolio/static/css/main.3817bfa4.chunk.css"
+    "revision": "555ca855da140834ad43",
+    "url": "/portfolio/static/css/main.944f63ec.chunk.css"
   },
   {
     "revision": "2acdd0c8e66dd51de5e1",
     "url": "/portfolio/static/js/2.fd217afb.chunk.js"
   },
   {
-    "revision": "bdd3ff9ef296bcf7e793",
+    "revision": "555ca855da140834ad43",
     "url": "/portfolio/static/js/main.12213a29.chunk.js"
   },
   {
