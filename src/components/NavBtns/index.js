b64c68aa54f5f86b63cd6c7c41bfe1cc4aa6cdae
David Weid <david.weid.2@gmail.com>
2019-05-26 01:10:16 -0400

    NavBtns now have visual feedback when active

--- src/components/NavBtns/index.js
+++ src/components/NavBtns/index.js
@@ -6,7 +6,7 @@ export function NavBtn(props) {
   return (
     <div type="button" className="btn-container" align="center">
       <Link
-        className="btn-link"
+        className={props.active ? "btn-link btn-disabled" : "btn-link"}
         to={`/${props.destination}`}
         onClick={props.active ? e => e.preventDefault() : null}
       >
