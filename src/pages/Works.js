7b65d1846583fa18f8a5ffd1029ed2c034a0b412
David Weid <david.weid.2@gmail.com>
2019-05-27 12:20:43 -0400

    Working on 'Works' page. Creating layout with dummy card divs. Using flexbox for responsiveness.

--- src/pages/Works.js
+++ src/pages/Works.js
@@ -1,11 +1,24 @@
 import React from "react";
 import { NavBtn } from "../components/Btns";
+import "./works.scss";
 
 const WorksPage = props => {
   return (
     <div className="page-works">
       <NavBtn className="nav-works" destination="" text="Home" />
       <NavBtn className="nav-contact" destination="contact" text="Contact" />
+      <div className="content-works">
+        {/* <div className="content-workstitle">Portfolio</div> */}
+        <div className="content-worksbody">
+            <div className="project-card project-featured">Item 1</div>
+            <div className="project-card">Item 2</div>
+            <div className="project-card">Item 3</div>
+            <div className="project-card">Item 4</div>
+            <div className="project-card">Item 5</div>
+            <div className="project-card">Item 6</div>
+            <div className="project-card">Item 7</div>
+        </div>
+      </div>
     </div>
   );
 };
