11ef04a94d652327c885ed993c7526aaf1d560a7
David Weid <david.weid.2@gmail.com>
2019-05-26 23:12:02 -0400

    Home page mostly completed.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,18 @@ const HomePage = props => {
       <NavBtn className="nav-about" destination="about" text="About" />
       <NavBtn className="nav-works" destination="works" text="Works" />
       <NavBtn className="nav-contact" destination="contact" text="Contact" />
+      <div className="content-home">
+        <div className="content-title">Welcome</div>
+        <div className="content-body">
+          <p>
+            My name is <span id="name">David J. Weid II</span>
+          </p>
+          <p>
+            I am a certified Full Stack <span>Web Developer</span> with <br />a
+            genuine <span>passion</span> to never stop learning and coding.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
